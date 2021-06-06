3452395c2827bfa0c991a741be68f3c285daf6ac
Shahid Siddique <ssiddique.ms@gmail.com>
2021-06-06 08:09:33 -0500

    remove unused javascript

--- jCart/checkout.php
+++ jCart/checkout.php
@@ -21,17 +21,6 @@ $cart =& $_SESSION['jcart']; if(!is_object($cart)) $cart = new jcart();
 		<link rel="stylesheet" type="text/css" media="screen, projection" href="demo.css" />
 	</head>
 	<body>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8810347615083175"
-     crossorigin="anonymous"></script>
-<ins class="adsbygoogle"
-     style="display:block; text-align:center;"
-     data-ad-layout="in-article"
-     data-ad-format="fluid"
-     data-ad-client="ca-pub-8810347615083175"
-     data-ad-slot="9435784898"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
 
 		<div id="wrapper">
 			<div id="sidebar">
@@ -51,17 +40,6 @@ $cart =& $_SESSION['jcart']; if(!is_object($cart)) $cart = new jcart();
 
 		<script type="text/javascript" src="jcart/jquery-1.3.2.min.js"></script>
 		<script type="text/javascript" src="jcart/jcart-javascript.min.php"></script>
-	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8810347615083175"
-     crossorigin="anonymous"></script>
-<ins class="adsbygoogle"
-     style="display:block; text-align:center;"
-     data-ad-layout="in-article"
-     data-ad-format="fluid"
-     data-ad-client="ca-pub-8810347615083175"
-     data-ad-slot="9435784898"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-187022639-1"></script>
 <script>
--- jCart/index.php
+++ jCart/index.php
@@ -21,17 +21,7 @@ $cart =& $_SESSION['jcart']; if(!is_object($cart)) $cart = new jcart();
 		<link rel="stylesheet" type="text/css" media="screen, projection" href="demo.css" />
 	</head>
 	<body>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8810347615083175"
-     crossorigin="anonymous"></script>
-<ins class="adsbygoogle"
-     style="display:block; text-align:center;"
-     data-ad-layout="in-article"
-     data-ad-format="fluid"
-     data-ad-client="ca-pub-8810347615083175"
-     data-ad-slot="9435784898"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+
 
 		<div id="wrapper">
 
@@ -140,19 +130,6 @@ $cart =& $_SESSION['jcart']; if(!is_object($cart)) $cart = new jcart();
 			</div>
 		</div>
 
-		<script type="text/javascript" src="jcart/jquery-1.3.2.min.js"></script>
-		<script type="text/javascript" src="jcart/jcart-javascript.min.php"></script>
-	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8810347615083175"
-     crossorigin="anonymous"></script>
-<ins class="adsbygoogle"
-     style="display:block; text-align:center;"
-     data-ad-layout="in-article"
-     data-ad-format="fluid"
-     data-ad-client="ca-pub-8810347615083175"
-     data-ad-slot="9435784898"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-187022639-1"></script>
 <script>
